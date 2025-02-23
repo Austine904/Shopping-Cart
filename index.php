7b194e1efb5bfa2dc68aeacedcb2ed3bbd78eddf
Austine904 <ochiengaustine89@gmail.com>
2025-02-23 17:40:20 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+
+    <!-- css file -->
+     <link rel="stylesheet" href="css/style.css">
+     
+     <!-- font awesome link -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+<body>
+    <!--header -->
+    <header class="header">
+    <div class="header_body">
+        <a href="index.php" class="logo">Mellow SuperMarket</a>
+        <nav class="navbar">
+            <a href="">Add Product</a>
+            <a href="">View Product</a>
+            <a href="">Shop It</a>
+        </nav>
+
+        <!-- shopping cart Icon -->
+         <a href="" class="cart"><i class="fa-solid fa-cart-shopping"></i><span><sup>3</sup></span></a>
+         <!-- <div id="menu_btn" class="fas fa-bars"></div> -->
+
+
+    </div>
+    </header>
+
+<!-- js file -->
+ <script src="js/script.js"></script>
+</body>
+</html>
