534bf6a524df197b2d4cfa6e74240f4ca9fe71e1
Austine904 <ochiengaustine89@gmail.com>
2025-02-27 03:55:40 +0300

    Cart Section last commit update

--- shop_products.php
+++ shop_products.php
@@ -2,10 +2,26 @@
  if (isset($_POST['add_to_cart'])) {
     $product_name=$_POST['name'];
     $product_price=$_POST['price'];
-    $prooduct_image=$_POST['image'];
+    $product_image=$_POST['image'];
+    $product_quantity=1;
+
+    //select cart data based on condition
+    $select_cart=mysqli_query($conn, "SELECT * FROM cart where name='$product_name'");
+     if(mysqli_num_rows($select_cart)>0){
+
+        
+        $display_message[]="product already added to cart!";
+     }else{
+ 
+ //insert cart data in cart table
+
+ $insert_products=mysqli_query($conn, "INSERT INTO cart (name,price,image,quantity) values('$product_name', '$product_price', '$product_image', '$product_quantity')");
+ 
+$display_message[]="product added to cart!";
+
+}
  }
-
-
+ 
 ?>
 
 
@@ -26,22 +42,56 @@
 <body>
     <!-- Header-->
      <?php include 'header.php' ?>
+
+     
      <div class="container">
+
+          <?php 
+     if(isset($display_message)){
+        foreach( $display_message as $display_message){
+        echo "<div class='display_message'>
+        <span>$display_message</span>
+        <i class='fas fa-times' onclick='this.parentElement.style.display=`none`';></i>
+     </div>";
+    }
+
+}
+     ?>
+
+
         <section class="products">
-            <hi class="heading">Let's Shop</hi>
+            <h1 class="heading">Let's Shop</h1>
             <div class="product_container">
-                <form action="" method="post">
+               
+  
+               
+               <?php
+
+                $select_product=mysqli_query($conn, "SELECT * FROM products");
+
+                if(mysqli_num_rows($select_product)>0){
+
+                    while($fetch_product=mysqli_fetch_assoc($select_product)){
+                       // echo $fetch_product['name'];
+                        ?>
+                     <form action="" method="post">
                     <div class="edit_form">
-                        <img src="images/Austine.png" alt="">
-                        <h3>Austine</h3>
-                        <div class="price">Price:4269</div>
-                        <input type="hidden" name='name'>
-                        <input type="hidden"name='price'>
-                        <input type="text"name='image'>
-                        <input type="submit" class="submit_btn cart_btn" value="Add to Cart">
+                        <img src="images/<?php echo $fetch_product['image'] ?>" alt="">
+                        <h3><?php echo $fetch_product['name'] ?></h3>
+                        <div class="price"><?php echo $fetch_product['price'] ?>/-</div>
+                        <input type="hidden" name="name" value="<?php echo $fetch_product['name'] ?>">
+                        <input type="hidden"name="price" value="<?php echo $fetch_product['price'] ?>">
+                        <input type="hidden"name="image"value= "<?php echo $fetch_product['image'] ?>">
+                        <input type="submit" name= "add_to_cart" class="submit_btn cart_btn" value="Add to Cart">
                     </div>
                 </form>
-            </div>
+                <?php
+               
+                    }
+                }else{
+                    echo "<div class='empty_text'>No products Available</div>";
+                } ?>
+               
         </section>
      </div>
 </body>
